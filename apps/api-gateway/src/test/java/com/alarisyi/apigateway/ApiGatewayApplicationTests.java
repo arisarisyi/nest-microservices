0f26cfe1644f43a86700f917d471916df6902716
arisarisyi <alarisyi@gmail.com>
2025-04-11 14:20:19 +0800

    create service Api gateway with spring

---
+++ apps/api-gateway/src/test/java/com/alarisyi/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alarisyi.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
